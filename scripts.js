18762980c228d5f08eaddebb0b6d2b4c9fe5afcd
uminocelo <marcelo.mdl.dev@gmail.com>
2025-01-03 01:52:49 -0300

    add duration time

--- scripts.js
+++ scripts.js
@@ -2,6 +2,9 @@ const html = document.querySelector('html');
 const btnFoco = document.querySelector('.app__card-button--foco');
 const btnCurto = document.querySelector('.app__card-button--curto');
 const btnLongo = document.querySelector('.app__card-button--longo');
+const duracaoFoco = 1500; 
+const duracaoDescansoCurto = 300; 
+const duracaoDescansoLongo = 900; 
 
 // Eventos
 btnFoco.addEventListener('click', () => {
