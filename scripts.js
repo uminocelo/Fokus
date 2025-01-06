50d7f8689a5889a9e85c13ef79313bf2ae0cfeaf
uminocelo <marcelo.mdl.dev@gmail.com>
2025-01-06 15:22:37 -0300

    change theme by context

--- scripts.js
+++ scripts.js
@@ -1,20 +1,42 @@
+// Variables
 const html = document.querySelector('html');
+
 const btnFoco = document.querySelector('.app__card-button--foco');
 const btnCurto = document.querySelector('.app__card-button--curto');
 const btnLongo = document.querySelector('.app__card-button--longo');
+const appImage = document.querySelector('.app__image');
+const appTitle = document.querySelector('.app__title');
+
 const duracaoFoco = 1500; 
 const duracaoDescansoCurto = 300; 
 const duracaoDescansoLongo = 900; 
 
 // Eventos
 btnFoco.addEventListener('click', () => {
-  html.setAttribute('data-contexto', 'foco');
+  changeContext('foco');
 });
 
 btnCurto.addEventListener('click', () => {
-  html.setAttribute('data-contexto', 'descanso-curto');
+  changeContext('descanso-curto');
 });
 
 btnLongo.addEventListener('click', () => {
-  html.setAttribute('data-contexto', 'descanso-longo');
+  changeContext('descanso-longo');
 });
+
+function changeContext(context) {
+  html.setAttribute('data-contexto', context);
+  appImage.setAttribute('src', `/imagens/${context}.png`);
+
+  switch(context){
+    case 'foco':
+      appTitle.innerHTML = `Otimize sua produtividade,<br><strong class="app__title-strong">mergulhe no que importa.</strong>`;
+      break;
+    case 'descanso-curto':
+      appTitle.innerHTML = `Que tal dar uma respirada? <br><strong class="app__title-strong">Faça uma pausa curta.</strong>`; 
+      break;
+    case 'descanso-longo':
+      appTitle.innerHTML = `Hora de voltar para a superficie. <br><strong class="app__title-strong">Faça uma pausa longa.</strong>`; 
+      break;
+  }
+}
